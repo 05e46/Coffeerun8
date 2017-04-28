7193c28b806b7b18a21908d5e583796fc8e53092
Felipe Perez <phillip.perez44@gmail.com>
2017-04-28 13:53:38 -0700

    fixed spacing linter isssues

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,41 +1,39 @@
 (function (window) {
-  'use strict';
+    'use strict';
 
-  var App = window.App || {};
-  var Promise = window.Promise;
+    var App = window.App || {};
+    var Promise = window.Promise;
 
-  function DataStore() {
-    console.log('running the DataStore function')
-    this.data = {};
-  }
+    function DataStore() {
+        console.log('running the DataStore function');
+        this.data = {};
+    }
 
-  function promiseResolvedWith(value) {
-    var promise = new Promise(function (resolve, reject) {
-      resolve(value);
-    });
-    return promise;
-  }
+    function promiseResolvedWith(value) {
+        var promise = new Promise(function (resolve, reject) {
+            resolve(value);
+        });
+        return promise;
+    }
 
-  DataStore.prototype.add = function (key, val) {
-    this.data[key] = val;
-    return promiseResolvedWith(null);
-  };
+    DataStore.prototype.add = function (key, val) {
+        this.data[key] = val;
+        return promiseResolvedWith(null);
+    };
 
-  DataStore.prototype.get = function (key) {
-    return promiseResolvedWith(this.data[key]);
-  };
+    DataStore.prototype.get = function (key) {
+        return promiseResolvedWith(this.data[key]);
+    };
 
-  DataStore.prototype.getAll = function () {
-    return promiseResolvedWith(this.data);
-  };
+    DataStore.prototype.getAll = function () {
+        return promiseResolvedWith(this.data);
+    };
 
-  DataStore.prototype.remove = function (key) {
-    delete this.data[key];
-    return promiseResolvedWith(null);
-  };
+    DataStore.prototype.remove = function (key) {
+        delete this.data[key];
+        return promiseResolvedWith(null);
+    };
 
-  App.DataStore = DataStore;
-  window.App = App;
+    App.DataStore = DataStore;
+    window.App = App;
 })(window);
-
-
